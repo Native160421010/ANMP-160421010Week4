6c9e400cd7b74366c0195177aa3ea0122e6d5923
Pootish2 <160823747+Pootish2@users.noreply.github.com>
2024-03-18 12:38:53 +0700

    Changes 4
    The End?

--- app/src/main/java/com/wildfire/adv160421010week4/view/StudentListFragment.kt
+++ app/src/main/java/com/wildfire/adv160421010week4/view/StudentListFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.wildfire.adv160421010week4.R
+import com.wildfire.adv160421010week4.databinding.FragmentStudentListBinding
+import com.wildfire.adv160421010week4.viewmodel.ListViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,25 +23,45 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class StudentListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewModel: ListViewModel
+    private val studentListAdapter  = StudentListAdapter(arrayListOf())
+    private lateinit var binding: FragmentStudentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_list, container, false)
+        binding = FragmentStudentListBinding.inflate(inflater,container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        binding.recView.layoutManager = LinearLayoutManager(context)
+        binding.recView.adapter = studentListAdapter
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.recView.visibility = View.GONE
+                binding.progressLoad.visibility = View.VISIBLE
+            } else {
+                binding.recView.visibility = View.VISIBLE
+                binding.progressLoad.visibility = View.GONE
+            }
+        })
+
+
+
+    }
+
 
     companion object {
         /**
